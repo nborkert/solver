26c805e4aac332158e01a59ef90dbe37fa968022
Neal Borkert <neal.borkert@gmail.com>
2014-11-08 15:49:36 -0500

    testing decimals now, too

--- playerPopulation_test.go
+++ playerPopulation_test.go
@@ -14,5 +14,14 @@ func TestAddPlayer(t *testing.T) {
 		t.Errorf("Expected 18, recieved %v", points)
 	}
 
+	player2 := "RB,Yo,SEA,32.33,40000"
 
+	newPositionAdded, newPoints := AddPlayer(player2)
+	if newPositionAdded != "RB" {
+		t.Error("Expected RB, received %v", newPositionAdded)
+	}
+	if newPoints != 32.33 {
+		t.Error("Expected 32.33, received %v", newPoints)
+	}
+	
 }
