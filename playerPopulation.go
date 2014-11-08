5a83b5595bfb39d60ed7bc5b40d1e3bab6e61e17
Neal Borkert <neal.borkert@gmail.com>
2014-11-08 14:11:13 -0500

    fixed package in webhandler and added playerPopulation.go

---
+++ playerPopulation.go
@@ -0,0 +1,12 @@
+package solver
+
+type Player struct {
+	PlayerName string
+	Position string
+	Salary int
+	ProjectedPoints int
+}
+
+func addPlayer (dataLine string) {
+	//Expect format QB,Peyton Manning,10,30000
+}
--- webhandler.go
+++ webhandler.go
@@ -1,4 +1,4 @@
-package webhandler
+package solver
 
 import (
     "fmt"
