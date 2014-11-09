67f600e9573bd73e0d99e5323c8c229dd1464124
Neal Borkert <neal.borkert@gmail.com>
2014-11-09 14:13:50 -0500

    works so far

--- roster.go
+++ roster.go
@@ -1,8 +1,8 @@
 package solver
 
-type Roster struct {
-	Players []Player
-}
+import (
+	"fmt"
+)
 
 func HowManyAtPosition(position string) int {
 	return len(playersByPosition[position])
@@ -16,12 +16,36 @@ func GetPositions() []string {
 	return positions
 }
 
-//Returns int of number of rosters created from entire population without validation of constraints
-func CreateRosters() int {
-	//loop over all keys of player population positions and players of those positions
-	//for position := range playersByPosition {
+func CreateRosters() {
+	c := make(chan []Player) //c is the channel used to send rosters to later processing
+	for position := range playersByPosition {
+		for _, player := range playersByPosition[position] {
+			fmt.Printf("About to call CreateRostersForRootNode for player %v\n", player)
+			go CreateRostersForRootNode(player, c)
+		}
+		break  //only needed that first entry from playersByPosition
+	}
+	ValidateRoster(c)
+	ValidateRoster(c)
+}
 
-	//}
-	return 0
+func CreateRostersForRootNode(rootNode Player, c chan []Player) {
+	fmt.Printf("Creating rosters for root %v\n", rootNode)
+
+	roster := make([]Player, 0)
+	roster = append(roster, rootNode)
+
+	fmt.Printf("About to call ValidateRoster for roster %v\n", roster)
+	//Send to channel for each possible roster
+	c <- roster
+}
+
+//This method is the centralized "pulling" function that evaluates all possible rosters
+//and finds the highest projected winner subject to the expressed constraints
+func ValidateRoster(c chan []Player) {
+	fmt.Printf("In ValidateRoster\n")
+	roster := <-c
+
+	fmt.Printf("Validating roster %v\n", roster)
 }
 
