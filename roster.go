e2c8d451f85bbcfd42f9c029f4bed037ca6f8e26
Neal Borkert <neal.borkert@gmail.com>
2014-11-10 13:40:51 -0500

    returns winning roster, removed fmt.Printf's

--- roster.go
+++ roster.go
@@ -1,50 +1,50 @@
 package solver
 
 import (
-	"fmt"
+//	"fmt"
 	"time"
 )
 
-func CreateRosters() {
+func CreateRosters() []Player {
 	c := make(chan []Player) //c is the channel used to send rosters to later processing
 	workComplete := make(chan int) //workComplete is the channel used to send an int to indicate
 		//that all candidate rosters have been sent from a given goroutine
 	var i = 0 //will be used to count the number of goroutines launched
 
 	for _, player := range AllPlayers[0] {
-		fmt.Printf("About to call CreateRostersForRootNode for player %v\n", player)
+//		fmt.Printf("About to call CreateRostersForRootNode for player %v\n", player)
 		go CreateRostersForRootNode(player, c, workComplete)
 		i++
 	}
-	FindWinningRoster(c, workComplete, i)
+	return FindWinningRoster(c, workComplete, i)
 }
 
 
 func CreateRostersForRootNode(rootNode Player, c chan []Player, workComplete chan int) {
-	fmt.Printf("Creating rosters for root %v\n", rootNode)
+//	fmt.Printf("Creating rosters for root %v\n", rootNode)
 
 	rootRoster := make([]Player, 0)
 	rootRoster = append(rootRoster, rootNode)
-	fmt.Printf("Root roster = %v\n", rootRoster)
+	//fmt.Printf("Root roster = %v\n", rootRoster)
 
 	var previousRosters [][]Player
 	previousRosters = append(previousRosters, rootRoster)
-	fmt.Printf("previousRosters = %v\n", previousRosters)
+	//fmt.Printf("previousRosters = %v\n", previousRosters)
 
 	//Start at row[1] of AllPlayers since we are given the root node in this function
 	for level := 1; level < len(AllPlayers); level++ {
-		fmt.Printf("level = %v\n", level)
+	//	fmt.Printf("level = %v\n", level)
 		var newRosters [][]Player
 		for _, player := range AllPlayers[level] {
-			fmt.Printf("PLAYER %v\n", player)
+	//		fmt.Printf("PLAYER %v\n", player)
 			//Now grab previously created rosters and add their players to newRoster
 			for previousRoster := range previousRosters {
 				newRoster := append(previousRosters[previousRoster], player)
-				fmt.Printf("NEW ROSTER AFTER ADDING PREVIOUS GUYS %v\n", newRoster)
+		//		fmt.Printf("NEW ROSTER AFTER ADDING PREVIOUS GUYS %v\n", newRoster)
 				newRosters = append(newRosters, newRoster)
 			}
 		}
-		fmt.Printf("newRosters = %v\n", newRosters)
+		//fmt.Printf("newRosters = %v\n", newRosters)
 		previousRosters = newRosters
 	}
 	//Validate roster composition and salary cap info
@@ -67,32 +67,32 @@ func CreateRostersForRootNode(rootNode Player, c chan []Player, workComplete cha
 
 //This method is the centralized "pulling" function that evaluates all possible rosters
 //and finds the highest projected winner subject to the expressed constraints
-func FindWinningRoster(c chan []Player, workComplete chan int, waitForWorkerCount int) {
+func FindWinningRoster(c chan []Player, workComplete chan int, waitForWorkerCount int) []Player {
 	completedWorkers := 0
 	var highestPoints float64 = 0.0
 	var winningRoster []Player
 	for {
 		select {
 		case roster := <-c:
-			fmt.Printf("Checking for winner for roster %v\n", roster)
+			//fmt.Printf("Checking for winner for roster %v\n", roster)
 			rosterPoints := PointsForRoster(roster)
-			fmt.Printf("Roster has points = %v\n", rosterPoints)
+			//fmt.Printf("Roster has points = %v\n", rosterPoints)
 			if rosterPoints > highestPoints {
-				fmt.Printf("HIGHEST SO FAR\n")
+			//	fmt.Printf("HIGHEST SO FAR\n")
 				highestPoints = rosterPoints
 				winningRoster = roster
 			}
 			//Now compare and keep winning roster to send back
 		case done := <-workComplete:
 			completedWorkers = completedWorkers + done
-			fmt.Printf("completedWorkers = %v\n", completedWorkers)
+			//fmt.Printf("completedWorkers = %v\n", completedWorkers)
 			if waitForWorkerCount == completedWorkers {
-				fmt.Printf("WINNING ROSTER = %v\n", winningRoster)
-				return
+			//	fmt.Printf("WINNING ROSTER = %v\n", winningRoster)
+				return winningRoster
 			}
-		case <-time.After(time.Second * 10):
-			fmt.Printf("Timed out\n")
-			return
+		case <-time.After(time.Second * 3600):
+			//fmt.Printf("Timed out\n")
+			return nil
 		}
 	}
 }
--- validateRoster.go
+++ validateRoster.go
@@ -1,7 +1,7 @@
 package solver
 
 import (
-	"fmt"
+//	"fmt"
 )
 
 var salaryCap int64 = 60000
@@ -9,7 +9,7 @@ var salaryCap int64 = 60000
 //Insure a player is not on the roster twice and that the cost of the roster is under or equal to the salary cap
 //Returns roster if valid, nil if not valid
 func ValidateRoster(roster []Player) []Player {
-	fmt.Printf("Validating roster %v\n", roster)
+//	fmt.Printf("Validating roster %v\n", roster)
 	dupTest := NoDuplicatePlayersFound(roster)
 	if !dupTest {
 		return nil
@@ -37,7 +37,7 @@ func NoDuplicatePlayersFound(roster []Player) bool {
 }
 
 func UnderSalaryCap(roster []Player, cap int64) bool {
-	fmt.Printf("Checking roster salary against cap of %v\n", cap)
+//	fmt.Printf("Checking roster salary against cap of %v\n", cap)
 	var total int64
 	total = 0
 	for _, player := range roster {
@@ -48,5 +48,4 @@ func UnderSalaryCap(roster []Player, cap int64) bool {
 	}
 	return true
 }
-
 
