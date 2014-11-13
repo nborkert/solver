f69bdddda1fe1eb98c3b6869ca9d2b65e044a218
Neal Borkert <neal.borkert@gmail.com>
2014-11-12 22:21:52 -0500

    Fixed

--- roster.go
+++ roster.go
@@ -2,7 +2,7 @@ package solver
 
 import (
 	"time"
-	"fmt"
+	//"fmt"
 )
 
 func CreateRosters() []Player {
@@ -24,43 +24,34 @@ func CreateRostersForRootNode(rootNode Player, c chan []Player, workComplete cha
 	rootRoster := make([]Player, 0)
 	rootRoster = append(rootRoster, rootNode)
 
-fmt.Printf("AllPlayers = %v\n", AllPlayers)
-
 	var previousRosters [][]Player
 	previousRosters = append(previousRosters, rootRoster)
 	//Start at row[1] of AllPlayers since we are given the root node in this function
 	for level := 1; level < len(AllPlayers); level++ {
-		//var newRosters [][]Player
-		newRosters := make([][]Player, 0)
+		newRosters := make([][]Player, 0)  //newRosters will be built,
+			//then will replace previousRosters on each iteration.
+
 		for _, player := range AllPlayers[level] {
-			fmt.Printf("Len of newRosters should be zero every time: %v\n", len(newRosters))
-			fmt.Printf("Len of previousRosters should never be zero: %v\n", len(previousRosters))
-	//		fmt.Printf("Adding player %v to previous rosters\n", player)
-			//Now grab previously created rosters and add their players to newRoster
-			fmt.Printf("Now adding this player to previous rosters: %v\n", player)
 			for i := range previousRosters {
-				fmt.Printf("previousRoster = %v\n", previousRosters[i])
-				newRoster := append(previousRosters[i], player)
-	//			fmt.Printf("Appended player %v to new roster\n", player)
+				//Below line was actually overwriting the previousRosters array while creating 
+				//newRoster
+				//newRoster := append(previousRosters[i], player)
+				newRoster := make([]Player, 1)
+				newRoster[0] = player
+				newRoster = append(newRoster, previousRosters[i]...)
 				newRosters = append(newRosters, newRoster)
-				fmt.Printf("newRosters are now %v\n", newRosters)
 			}
 		}
-		previousRosters = make([][]Player, 0)  //clean out previousRosters
 		previousRosters = newRosters
-		fmt.Printf("previousRosters are now %v\n", previousRosters)
-
 	}
 	//Validate roster composition and salary cap info
 	var validRosters [][]Player
 	for _, roster := range previousRosters {
-		fmt.Printf("Roster to validate %v\n", roster)
 		isValidRoster := ValidateRoster(roster)
 		if isValidRoster != nil {
 			validRosters = append(validRosters, isValidRoster)
 		}
 	}
-	fmt.Printf("Found number of valid rosters for %v - %v\n", rootNode, len(validRosters))
 	//Send each valid roster to channel to find winner
 	for _, validRosterMightWin := range validRosters {
 		c <- validRosterMightWin
