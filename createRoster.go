1eaf868a7fc9bbba50273e0f305efb0182e7f36d
Neal Borkert <neal.borkert@gmail.com>
2014-11-23 10:16:22 -0500

    rearragned code, no functionality change

--- createRoster.go
+++ createRoster.go
@@ -1,7 +1,6 @@
 package solver
 
 import (
-	"time"
 	//"fmt"
 )
 
@@ -62,36 +61,3 @@ func CreateRostersForRootNode(rootNode Player, c chan []Player, workComplete cha
 }
 
 
-//This method is the centralized "pulling" function that evaluates all possible rosters
-//and finds the highest projected winner subject to the expressed constraints
-func FindWinningRoster(c chan []Player, workComplete chan int, waitForWorkerCount int) []Player {
-	completedWorkers := 0
-	var highestPoints float64 = 0.0
-	var winningRoster []Player
-	for {
-		select {
-		case roster := <-c:
-			rosterPoints := PointsForRoster(roster)
-			//Now compare and keep winning roster to send back
-			if rosterPoints > highestPoints {
-				highestPoints = rosterPoints
-				winningRoster = roster
-			}
-		case done := <-workComplete:
-			completedWorkers = completedWorkers + done
-			if waitForWorkerCount == completedWorkers {
-				return winningRoster
-			}
-		case <-time.After(time.Second * 36000): //Ten hours timeout value
-			return nil
-		}
-	}
-}
-
-func PointsForRoster(roster []Player) float64 {
-	var points float64 = 0.0
-	for _, player := range roster {
-		points += player.ProjectedPoints
-	}
-	return points
-}
--- roster.go
+++ roster.go
@@ -1,5 +1,9 @@
 package solver
 
+import (
+	"time"
+)
+
 
 // A Roster is a list of Player structs and their combined salary
 type Roster struct {
@@ -9,3 +13,36 @@ type Roster struct {
 
 
 
+//This method is the centralized "pulling" function that evaluates all possible rosters
+//and finds the highest projected winner subject to the expressed constraints
+func FindWinningRoster(c chan []Player, workComplete chan int, waitForWorkerCount int) []Player {
+	completedWorkers := 0
+	var highestPoints float64 = 0.0
+	var winningRoster []Player
+	for {
+		select {
+		case roster := <-c:
+			rosterPoints := PointsForRoster(roster)
+			//Now compare and keep winning roster to send back
+			if rosterPoints > highestPoints {
+				highestPoints = rosterPoints
+				winningRoster = roster
+			}
+		case done := <-workComplete:
+			completedWorkers = completedWorkers + done
+			if waitForWorkerCount == completedWorkers {
+				return winningRoster
+			}
+		case <-time.After(time.Second * 36000): //Ten hours timeout value
+			return nil
+		}
+	}
+}
+
+func PointsForRoster(roster []Player) float64 {
+	var points float64 = 0.0
+	for _, player := range roster {
+		points += player.ProjectedPoints
+	}
+	return points
+}
